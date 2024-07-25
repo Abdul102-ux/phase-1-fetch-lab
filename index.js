76ed831dcaa504d74390d89f1033ef278c54502b
abdul khaliq <abdallamakhtal@gmail.com>
2024-07-25 18:23:51 +0300

    solution

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+      .then((response) => response.json())
+      .then((data) => renderBooks(data))
+      .catch((error) => console.error('Error fetching books:', error));
+}
+
   // To pass the tests, don't forget to return your fetch!
   
-}
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
